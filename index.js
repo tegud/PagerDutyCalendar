ec53f3b46d8bf8e9bea7014b6aacf915f325005f
Steve Elliott <steve@tegud.net>
2014-12-19 17:46:25 +0000

    Can now deal with schedule overrides
    Code needs oh so much refactor

--- index.js
+++ index.js
@@ -5,15 +5,19 @@ var http = require('http');
 var async = require('async');
 var _ = require('lodash');
 var moment = require('moment');
+var Promise = require('bluebird');
 
 var AppServer = require('./lib/AppServer');
 var PagerDutyCalendar = require('./lib/PagerDutyClient/calendar.js');
 
-function getDateSet() {
+function getDateSet(start) {
     var dates = [];
     var today = moment();
-    var start = moment(today).startOf('month').add(-1, 'months');
-    var end = moment(start).add(3, 'months');
+    var niceStart = moment(today).startOf('month').add(-1, 'months');
+    if(!start) {
+        start = niceStart;
+    }
+    var end = moment(niceStart).add(3, 'months');
     var currentDate = moment(start);
 
     while(end.diff(currentDate) > 0) {
@@ -39,35 +43,75 @@ var server = function() {
     app.use("/static", express.static(applicationRoot + 'static'));
 
     app.get('/', function(req, res, next) {
-        calendar
-           .get('PNHU7IO')
-           .then(function(data) {
-                console.log(JSON.stringify(data ,null, 4));
+        var dateSet = getDateSet();
+
+        Promise.all([
+                calendar.get('PNHU7IO'),
+                calendar.getOverrides('PNHU7IO', dateSet[0].format('YYYY-MM-DD'), dateSet[dateSet.length - 1].format('YYYY-MM-DD'))
+            ])
+           .then(function(results) {
+                var data = results[0];
+                var overrides = _.map(results[1].overrides, function(override) {
+                    return {
+                        start: moment(override.start, 'YYYY-MM-DD'),
+                        end: moment(override.end, 'YYYY-MM-DD'),
+                        user: override.user
+                    }
+                });
+
+                console.log(JSON.stringify(overrides ,null, 4));
 
                 var scheduleLayers = data.schedule.schedule_layers;
 
                 var onCallLayer = _.chain(scheduleLayers).filter(function(layer) {
                     return _.filter(layer.users, function(user) {
-                        return user.name === 'ITSupport';
+                        return user.user.name === 'ITSupport';
                     }).length === 0;
                 }).first().value();
 
+                var rotationUsers = onCallLayer.users;
+                var rotationUserIds = _.map(rotationUsers, function(user) { return user.user.id; });
+                var allUsers = rotationUsers.concat(_.chain(overrides).pluck('user').groupBy('id').map(function(array, key) {
+                    return array[0];
+                }).filter(function(user) {
+                    return !_.contains(rotationUserIds, user.id);
+                }).map(function(user) {
+                    return { user: user };
+                }).value());
+
                 var layerStart = moment(onCallLayer.rotation_virtual_start, 'YYYY-MM-DD');
+                var rotationLengthInDays = onCallLayer.rotation_turn_length_seconds / 60 / 60 / 24;
+                var offSet = dateSet[0].diff(layerStart, 'days');
 
-                console.log(layerStart.format('DD MM YYYY'));
+                var onCallAllocation = _.map(dateSet, function(date, i) {
+                    var validOverride = _.chain(overrides).filter(function(override) {
+                        return date.diff(override.start) >= 0
+                            && date.diff(override.end) < 0;
+                    }).first().value();
+                    var onCallUserIndex = Math.floor((i + offSet) / rotationLengthInDays) % rotationUsers.length;
 
-                var dateSet = getDateSet();
+                    if(validOverride) {
+                        var matchedUser = _.chain(allUsers).map(function(user) { return user.user.id; }).indexOf(validOverride.user.id).value();
+
+                        onCallUserIndex = matchedUser;
+                    }
+
+                    return {
+                        date: date,
+                        index: i,
+                        onCall: onCallUserIndex
+                    };
+                });
+
+                function isOnCall(dateIndex, memberIndex) {
+                    return onCallAllocation[dateIndex].onCall === memberIndex;
+                }
 
                 var scheduleGroups = [
                     {
                         name: 'Level 1 - Duty Manager',
                         color: '#f00',
-                        members: [
-                            'Duty Manager 1',
-                            'Duty Manager 2',
-                            'Duty Manager 3',
-                            'Duty Manager 4'
-                        ]
+                        members: _.map(allUsers, function(user) { return user.user.name; })
                     },
                     {
                         name: 'Level 2 - Database Support',
@@ -118,6 +162,8 @@ var server = function() {
                     }
                 ];
 
+
+
                 var headers = _.map(scheduleGroups, function(group) {
                     return {
                         name: group.name,
@@ -137,15 +183,21 @@ var server = function() {
                     }));
                 }, []);
 
-                var rows = _.map(dateSet, function(date) {
+                var rows = _.map(dateSet, function(date, dateIndex) {
                     return {
                         date: date.format('ddd DD MMM YYYY'),
                         cells: _.reduce(scheduleGroups, function(memo, group) {
-                            return memo.concat(_.map(group.members, function(member, i) {
+                            return memo.concat(_.map(group.members, function(member, memberIndex) {
+                                var isOnCallThisDay = group.members.length === 1;
+
+                                if(group.name === 'Level 1 - Duty Manager') {
+                                    isOnCallThisDay = isOnCall(dateIndex, memberIndex);
+                                }
+
                                 return {
-                                    endOfGroup: i === group.members.length - 1,
+                                    endOfGroup: memberIndex === group.members.length - 1,
                                     cellColor: group.color,
-                                    isOnCall: group.members.length === 1
+                                    isOnCall: isOnCallThisDay
                                 };
                             }));
                         }, [])
