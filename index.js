15abf71fb365bddf5338845d1d99806e3182bd83
Steve Elliott <steve@tegud.net>
2014-12-23 21:39:09 +0000

    Splitting apart logic

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ var Promise = require('bluebird');
 
 var AppServer = require('./lib/AppServer');
 var PagerDutyCalendar = require('./lib/PagerDutyClient/calendar.js');
+var PagerDutyCalendarMapper = require('./lib/mappers/pagerduty.js');
+
+var scheduleMappers = {
+    default: require('./lib/mappers/default.js')
+}; 
 
 function getDateSet(start) {
     var dates = [];
@@ -36,7 +41,6 @@ var server = function() {
     var applicationRoot = __dirname + (process.env.NODE_ENV === 'dev' ? '/' : '/dist/');
     var credentials;
     var schedules;
-    var calendar;
 
     app.set('view engine', 'html');
     app.set('views', applicationRoot + 'views');
@@ -46,103 +50,6 @@ var server = function() {
     app.get('/', function(req, res, next) {
         var dateSet = getDateSet();
         var focusDate = moment().startOf('isoWeek');
-        var scheduleMappers = {
-            pagerduty: function(config, dates) {
-                return new Promise(function(resolve, reject) {
-                    Promise.all([
-                        calendar.get(config.scheduleId),
-                        calendar.getOverrides(config.scheduleId, dates[0].format('YYYY-MM-DD'), dates[dates.length - 1].format('YYYY-MM-DD'))
-                    ]).then(function(results) {
-                        var data = results[0];
-                        var overrides = _.map(results[1].overrides, function(override) {
-                            return {
-                                start: moment(override.start, 'YYYY-MM-DD'),
-                                end: moment(override.end, 'YYYY-MM-DD'),
-                                user: override.user
-                            }
-                        });
-
-                        var scheduleLayers = data.schedule.schedule_layers;
-                        var relevantLayer = _.chain(scheduleLayers).filter(function(layer) {
-                            return _.contains(config.layers, layer.id);
-                        }).first().value();
-                        var rotationUsers = relevantLayer.users;
-                        var rotationUserIds = _.map(rotationUsers, function(user) { return user.user.id; });
-                        var allUsers = rotationUsers.concat(_.chain(overrides).pluck('user').groupBy('id').map(function(array, key) {
-                            return array[0];
-                        }).filter(function(user) {
-                            return !_.contains(rotationUserIds, user.id);
-                        }).map(function(user) {
-                            return { user: user };
-                        }).value());
-                        var layerStart = moment(relevantLayer.rotation_virtual_start, 'YYYY-MM-DD');
-                        var rotationLengthInDays = relevantLayer.rotation_turn_length_seconds / 60 / 60 / 24;
-                        var offSet = dateSet[0].diff(layerStart, 'days');
-
-                        var onCallAllocation = _.map(dates, function(date, i) {
-                            var validOverride = _.chain(overrides).filter(function(override) {
-                                return date.diff(override.start) >= 0
-                                    && date.diff(override.end) < 0;
-                            }).first().value();
-                            var onCallUserIndex = Math.floor((i + offSet) / rotationLengthInDays) % rotationUsers.length;
-
-                            if(validOverride) {
-                                var matchedUser = _.chain(allUsers).map(function(user) { return user.user.id; }).indexOf(validOverride.user.id).value();
-
-                                onCallUserIndex = matchedUser;
-                            }
-
-                            return {
-                                date: date,
-                                index: i,
-                                onCall: onCallUserIndex
-                            };
-                        });
-
-                        resolve({
-                            name: config.name,
-                            color: config.color,
-                            members: _.map(allUsers, function(user, memberIndex) {
-                                return {
-                                    name: user.user.name,
-                                    email: user.user.email,
-                                    dates: _.map(dates, function(date, dateIndex) {
-                                        return {
-                                            date: date,
-                                            onCall: onCallAllocation[dateIndex].onCall === memberIndex
-                                        }
-                                    })
-                                };
-                            })
-                        });
-                    }, reject);
-                });
-            },
-            default: function(config, dates) {
-                var members = _.map(config.members, function(member) {
-                    return {
-                        name: member
-                    };
-                });
-
-                if(members.length === 1) {
-                    members[0].dates = _.map(dates, function(date) {
-                        return {
-                            date: date,
-                            onCall: true
-                        }
-                    });
-                }
-
-                return new Promise(function(resolve, reject) {
-                    resolve({
-                        name: config.name,
-                        color: config.color,
-                        members: members
-                    });
-                });
-            }
-        };
 
         Promise.all(_.map(schedules, function(scheduleConfig) {
                 return scheduleMappers[scheduleConfig.type || 'default'](scheduleConfig, dateSet);
@@ -167,7 +74,7 @@ var server = function() {
                             cellColor: group.color
                         };
                     }));
-                }, []).value();
+                }, []);
 
                 var rows = _.map(dateSet, function(date, dateIndex) {
                     return {
@@ -212,10 +119,7 @@ var server = function() {
                         credentials = JSON.parse(results[0]);
                         schedules = JSON.parse(results[1]);
 
-                        calendar = new PagerDutyCalendar({
-                            apiKey: credentials.apiKey,
-                            subDomain: credentials.subDomain
-                        });
+                        scheduleMappers.pagerduty = new PagerDutyCalendarMapper(credentials);
 
                         callback();
                     },
